9f10468190b4dfa0df407fa2e489ec37a9199480
Muzamelov Akezhan <akejanmuzamelov@gmail.com>
2026-01-20 20:45:58 +0600

    Akezhan: i have made 2 entities, membership type and class booking, we should write services for them later

--- src/entities/ClassBooking.java
+++ src/entities/ClassBooking.java
@@ -1,4 +1,24 @@
 package entities;
-
 public class ClassBooking {
+    private Member member;
+    private FitnessClass fitnessClass;
+
+    public ClassBooking(Member member, FitnessClass fitnessClass) {
+        setFitnessClass(fitnessClass);
+        setMember(member);
+    }
+
+    public FitnessClass getFitnessClass() {
+        return fitnessClass;
+    }
+    public void setFitnessClass(FitnessClass fitnessClass) {
+        this.fitnessClass = fitnessClass;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+    public void setMember(Member member) {
+        this.member = member;
+    }
 }
