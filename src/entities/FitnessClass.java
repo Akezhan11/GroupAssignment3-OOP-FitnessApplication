4c3aa20532edf1e3dd2cf20b6cb1321ed50da673
tauyekelulyshalkar <tauekelulyshalkar@gmail.com>
2026-01-20 18:57:04 +0500

    Shalkar ,I add Member and FitnessClass entities

--- src/entities/FitnessClass.java
+++ src/entities/FitnessClass.java
@@ -1,4 +1,31 @@
 package entities;
 
 public class FitnessClass {
+
+    private int id;
+    private String title;
+    private int capacity;
+
+    public FitnessClass(int id, String title, int capacity) {
+        this.id = id;
+        this.title = title;
+        this.capacity = capacity;
+    }
+
+    public FitnessClass(String title, int capacity) {
+        this.title = title;
+        this.capacity = capacity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
 }
--- src/entities/Member.java
+++ src/entities/Member.java
@@ -1,4 +1,44 @@
 package entities;
 
+import java.time.LocalDate;
+
 public class Member {
+
+    private int id;
+    private String name;
+    private String email;
+    private LocalDate membershipEnd;
+
+    public Member(int id, String name, String email, LocalDate membershipEnd) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.membershipEnd = membershipEnd;
+    }
+
+    public Member(String name, String email, LocalDate membershipEnd) {
+        this.name = name;
+        this.email = email;
+        this.membershipEnd = membershipEnd;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getMembershipEnd() {
+        return membershipEnd;
+    }
+
+    public boolean isMembershipActive() {
+        return membershipEnd.isAfter(LocalDate.now());
+    }
 }
