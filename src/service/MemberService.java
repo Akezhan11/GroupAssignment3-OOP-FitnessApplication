312cc6ec3ce26af2e898c2e475d13f35dd6d76b7
tauyekelulyshalkar <tauekelulyshalkar@gmail.com>
2026-01-29 16:13:26 +0500

    Shalkar ,Add MemberService

--- src/service/MemberService.java
+++ src/service/MemberService.java
@@ -1,4 +1,48 @@
 package service;
 
+import entities.Member;
+import exception.MemberNotFoundException;
+import repositories.MemberRepository;
+
+import java.util.List;
+
 public class MemberService {
+
+    private final MemberRepository repository;
+
+    public MemberService(MemberRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addMember(Member member) {
+        repository.save(member);
+    }
+
+    public Member getById(int id) {
+        Member member = repository.findById(id);
+        if (member == null) {
+            throw new MemberNotFoundException("Member not found: id=" + id);
+        }
+        return member;
+    }
+
+    public Member getByEmail(String email) {
+        Member member = repository.findByEmail(email);
+        if (member == null) {
+            throw new MemberNotFoundException("Member not found: email=" + email);
+        }
+        return member;
+    }
+
+    public Member getByPhone(String phone) {
+        Member member = repository.findByPhone(phone);
+        if (member == null) {
+            throw new MemberNotFoundException("Member not found: phone=" + phone);
+        }
+        return member;
+    }
+
+    public List<Member> getAllMembers() {
+        return repository.findAll();
+    }
 }
