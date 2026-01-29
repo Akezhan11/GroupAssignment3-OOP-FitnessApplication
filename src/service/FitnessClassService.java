3f2ec04ea9c624a2553c07199fe9e09dbf07e905
tauyekelulyshalkar <tauekelulyshalkar@gmail.com>
2026-01-29 16:10:42 +0500

    Shalkar ,Add FitnessClassService

--- src/service/FitnessClassService.java
+++ src/service/FitnessClassService.java
@@ -1,4 +1,27 @@
 package service;
 
+import entities.FitnessClass;
+import repositories.FitnessClassRepository;
+
+import java.util.List;
+
 public class FitnessClassService {
+
+    private final FitnessClassRepository repository;
+
+    public FitnessClassService(FitnessClassRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addClass(FitnessClass fitnessClass) {
+        repository.save(fitnessClass);
+    }
+
+    public List<FitnessClass> getAllClasses() {
+        return repository.findAll();
+    }
+
+    public FitnessClass getById(int id) {
+        return repository.findById(id);
+    }
 }
