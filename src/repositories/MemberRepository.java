9705f24c699a97960d828ea5af3ae0f8d12ed07d
tauyekelulyshalkar <tauekelulyshalkar@gmail.com>
2026-01-29 14:47:56 +0500

    Shalkar ,Аdd member repository methods

--- src/repositories/MemberRepository.java
+++ src/repositories/MemberRepository.java
@@ -1,7 +1,17 @@
 package repositories;
+
 import entities.Member;
 import java.util.List;
 
-public interface MemberRepository{
+public interface MemberRepository {
+
     void save(Member member);
+
+    Member findById(int id);
+
+    Member findByEmail(String email);
+
+    Member findByPhone(String phone);
+
+    List<Member> findAll();
 }
